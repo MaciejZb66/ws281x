943c21f7ae105edfa0813c6694144a48f399830b
Maciej Lenovo Mint <maciej.zbrzezny66@gmail.com>
2025-03-25 09:13:53 +0100

    Issues subsection added

--- effects.c
+++ effects.c
@@ -6,7 +6,7 @@ extern const uint8_t gamma8[];
 extern const uint8_t reverse_max_gamma8[];
 extern const uint8_t reverse_min_gamma8[];
 
-void move_one_led_down(WS281x_data* led){
+void WS281x_move_one_led_down(WS281x_data* led){
 	for(int i = 0; i <led->number_of_leds; i++)
 	{
 		if(i != 0){
@@ -17,7 +17,7 @@ void move_one_led_down(WS281x_data* led){
 	}
 }
 
-void bright_up(WS281x_data* led, uint8_t bright, uint8_t led_num){
+void WS281x_bright_up(WS281x_data* led, uint8_t bright, uint8_t led_num){
 	bright %= 101;
 	for(int i = 1; i <= 3; i++){
 		if(led->LED_info[led_num][i] * (100 + bright)/ 100 < 255){
@@ -29,7 +29,7 @@ void bright_up(WS281x_data* led, uint8_t bright, uint8_t led_num){
 
 }
 
-void bright_down(WS281x_data* led, uint8_t bright, uint8_t led_num){
+void WS281x_bright_down(WS281x_data* led, uint8_t bright, uint8_t led_num){
 	bright %= 101;
 	for(int i = 1; i <= 3; i++){
 		if(led->LED_info[led_num][i] * (100 - bright)/ 100 > 1){
@@ -41,19 +41,19 @@ void bright_down(WS281x_data* led, uint8_t bright, uint8_t led_num){
 
 }
 
-void bright_all_up(WS281x_data* led, uint8_t bright){
+void WS281x_bright_all_up(WS281x_data* led, uint8_t bright){
 	for(int i = 0; i < led->number_of_leds; i++){
-		bright_up(led, bright, i);
+		WS281x_bright_up(led, bright, i);
 	}
 }
 
-void bright_all_down(WS281x_data* led, uint8_t bright){
+void WS281x_bright_all_down(WS281x_data* led, uint8_t bright){
 	for(int i = 0; i < led->number_of_leds; i++){
-		bright_down(led, bright, i);
+		WS281x_bright_down(led, bright, i);
 	}
 }
 
-void one_walking_led_up(WS281x_data* led, uint16_t delay, uint8_t start, uint8_t end){
+void WS281x_one_walking_led_up(WS281x_data* led, uint16_t delay, uint8_t start, uint8_t end){
 	uint8_t colors[3];
 	if(end > led->number_of_leds){
 		end = led->number_of_leds;
@@ -73,7 +73,7 @@ void one_walking_led_up(WS281x_data* led, uint16_t delay, uint8_t start, uint8_t
 	}
 }
 
-void one_walking_led_down(WS281x_data* led, uint16_t delay, uint8_t start, uint8_t end){
+void WS281x_one_walking_led_down(WS281x_data* led, uint16_t delay, uint8_t start, uint8_t end){
 	uint8_t colors[3];
 	if(start > led->number_of_leds){
 			start = led->number_of_leds;
@@ -94,7 +94,7 @@ void one_walking_led_down(WS281x_data* led, uint16_t delay, uint8_t start, uint8
 
 }
 
-void rainbow(WS281x_data* led, uint8_t start, uint8_t end, uint8_t huejump, uint8_t hueoffset){
+void WS281x_rainbow(WS281x_data* led, uint8_t start, uint8_t end, uint8_t huejump, uint8_t hueoffset){
 	uint16_t hueef;
 	for(int j = start; j < end; j++){
 
--- effects.h
+++ effects.h
@@ -1,11 +1,11 @@
 #pragma once
 #include "led.h"
 
-void move_one_led_down(WS281x_data* led);
-void bright_up(WS281x_data* led, uint8_t bright, uint8_t led_num);
-void bright_down(WS281x_data* led, uint8_t bright, uint8_t led_num);
-void bright_all_up(WS281x_data* led, uint8_t bright);
-void bright_all_down(WS281x_data* led, uint8_t bright);
-void one_walking_led_up(WS281x_data* led, uint16_t delay, uint8_t start, uint8_t end);
-void one_walking_led_down(WS281x_data* led, uint16_t delay, uint8_t start, uint8_t end);
-void rainbow(WS281x_data* led, uint8_t start, uint8_t end, uint8_t huejump, uint8_t hueoffset);
+void WS281x_move_one_led_down(WS281x_data* led);
+void WS281x_bright_up(WS281x_data* led, uint8_t bright, uint8_t led_num);
+void WS281x_bright_down(WS281x_data* led, uint8_t bright, uint8_t led_num);
+void WS281x_bright_all_up(WS281x_data* led, uint8_t bright);
+void WS281x_bright_all_down(WS281x_data* led, uint8_t bright);
+void WS281x_one_walking_led_up(WS281x_data* led, uint16_t delay, uint8_t start, uint8_t end);
+void WS281x_one_walking_led_down(WS281x_data* led, uint16_t delay, uint8_t start, uint8_t end);
+void WS281x_rainbow(WS281x_data* led, uint8_t start, uint8_t end, uint8_t huejump, uint8_t hueoffset);
